a0244c161069d46692d1c1991afcd9f8b9a87534
Riko <113864095+riko0800r@users.noreply.github.com>
2022-09-24 10:45:03 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.rikos.Zaperword;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.rikos.Zaperword.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
---
+++ SWING.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SWING {
+  
+  
+  Scanner sc1 = new scanner(System.in);
+  System.out.println("digite o nome do personagem:");
+  sc1.nextLine();
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
